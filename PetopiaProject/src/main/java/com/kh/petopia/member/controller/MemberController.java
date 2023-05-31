b0eca07d23f2f7cbe014efb35cf2f60a8cbab240
hanyMK <hany.mi.kim@gmail.com>
2023-05-31 15:52:47 +0900

    commit- hany

--- PetopiaProject/src/main/java/com/kh/petopia/member/controller/MemberController.java
+++ PetopiaProject/src/main/java/com/kh/petopia/member/controller/MemberController.java
@@ -7,6 +7,14 @@ public class MemberController {
 		System.out.println("이제 진짜 인코딩 끝 ?");
 		
 		System.out.println("이거 괜찮겠지..?");
+		
+		System.out.println("dkdsud");
+		System.out.println("고마워");
+		
+		
+		System.out.println("e동물 농ㅈ장 화이티이딩ㅇ");
+		
+		
 	
 	}
 	
