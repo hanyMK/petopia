0265e3d5f64c97dbccbebb815584225d15ab9b3c
seohee0205 <134987067+seohee0205@users.noreply.github.com>
2023-05-31 11:45:04 +0900

    충돌 테스트 4 - 서희-

--- PetopiaProject/src/main/java/com/kh/petopia/board/controller/BoardController.java
+++ PetopiaProject/src/main/java/com/kh/petopia/board/controller/BoardController.java
@@ -5,6 +5,11 @@ public class BoardController {
 		System.out.println("안녕하세요");
 		
 		System.out.println("충돌 발생");
+		System.out.println("서히 막 써보는중");
+		System.out.println("서히 막 써보는중");
+		System.out.println("서히 막 써보는중");
+		System.out.println("서히 막 써보는중");
+		System.out.println("서히 막 써보는중");
 	}
 	
 	public void hehe() {
