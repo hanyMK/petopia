1671b0f732014c358a4231d9824f551d7a396162
abiccabi <93585123+abiccabi@users.noreply.github.com>
2023-06-01 19:09:33 +0900

    Update PageInfo.java

--- PetopiaProject/src/main/java/com/kh/petopia/common/model/vo/PageInfo.java
+++ PetopiaProject/src/main/java/com/kh/petopia/common/model/vo/PageInfo.java
@@ -2,9 +2,11 @@ package com.kh.petopia.common.model.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+
 @AllArgsConstructor
+@NoArgsConstructor
 public class PageInfo {
 
 	private int listCount;
