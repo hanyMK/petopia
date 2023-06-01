185bb5389f925c420eadc627c6476ec5381a1402
JeonTaeYoung <bymarch21@gmail.com>
2023-06-01 10:03:13 +0900

    마이페이지 이동 -지성-

--- PetopiaProject/src/main/java/com/kh/petopia/member/controller/MemberControllerJ.java
+++ PetopiaProject/src/main/java/com/kh/petopia/member/controller/MemberControllerJ.java
@@ -1,7 +1,20 @@
 package com.kh.petopia.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.petopia.member.model.service.MemberService;
+
+@Controller
 public class MemberControllerJ {
 	
-	fdafdasfd
+	@Autowired
+	private MemberService memberService;
+	
+	@RequestMapping("myPage.me")
+	public String myPage() {
+		return "member/myPage";
+	}
 
 }
