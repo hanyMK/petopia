1184aeff5e9cbe99dcae381570d89c3dd9bf3d9b
abiccabi <93585123+abiccabi@users.noreply.github.com>
2023-06-01 11:07:41 +0900

    admin 관련 -호-

--- PetopiaProject/src/main/java/com/kh/petopia/admin/controller/AdminController.java
+++ PetopiaProject/src/main/java/com/kh/petopia/admin/controller/AdminController.java
@@ -1,5 +1,17 @@
 package com.kh.petopia.admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.petopia.admin.model.service.AdminService;
+
+@Controller
 public class AdminController {
 
+	
+	@RequestMapping("admin.ad")
+	public String memberList() {
+		return "admin/adminMemberList";
+	}
 }
