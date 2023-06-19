616ff0308289e75a4d9a6741d37aacd1fb0d583a
abiccabi <93585123+abiccabi@users.noreply.github.com>
2023-06-19 12:43:21 +0900

    커밋 -호
    호호호

--- PetopiaProject/src/main/java/com/kh/petopia/admin/controller/AjaxAdminController.java
+++ PetopiaProject/src/main/java/com/kh/petopia/admin/controller/AjaxAdminController.java
@@ -1,6 +1,7 @@
 package com.kh.petopia.admin.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -117,4 +118,55 @@ public class AjaxAdminController {
 			return new Gson().toJson(result);
 
 		}
+	
+	@RequestMapping(value = "ajaxShippingRelease.ad")
+	public String ShippingRelease(int[] receiptNos) {
+		adminService.shippingRelease(receiptNos);
+		return "";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
