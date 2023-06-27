0682be4c06fa50af284f996657fceb85aa554df6
choi-jisung <bymarch21@gmail.com>
2023-06-27 16:02:33 +0900

    952952952

--- PetopiaProject/src/main/java/com/kh/petopia/petStore/model/service/PetStoreService.java
+++ PetopiaProject/src/main/java/com/kh/petopia/petStore/model/service/PetStoreService.java
@@ -10,7 +10,5 @@ public interface PetStoreService {
 	// 호텔, 유치원, 미용, 훈련 각각의 펫 스토어 정보를 SELECT하는 SELECT문 
 	PetStore selectPetStore(int psno);
 	
-	// 호텔, 유치원, 미용, 훈련 각각에 해당하는 리뷰리스트를 가져옴 
-	ArrayList<PetStoreReview> selectReviewList(int psno);
-
+	
 }
